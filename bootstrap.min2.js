df6bb1430c21c7ccfc9ecf8b9da7b526a27c825c
ashleydl <loof0032@hz.nl>
2018-07-29 20:32:26 +0200

    first commit

---
+++ bootstrap.min2.js
@@ -0,0 +1,13 @@
+/*!
+ * Bootstrap v3.3.7 (http://getbootstrap.com)
+ * Copyright 2011-2016 Twitter, Inc.
+ * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
+ */
+
+/*!
+ * Generated using the Bootstrap Customizer (http://getbootstrap.com/customize/?id=f46a1cc5a479f8768ad6340c56b94ea5)
+ * Config saved to config.json and https://gist.github.com/f46a1cc5a479f8768ad6340c56b94ea5
+ */
+ $('button').click(function() {
+   $(this).toggleClass('expanded').siblings('div').slideToggle();
+ });
